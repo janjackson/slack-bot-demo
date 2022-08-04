1e31eb80c0ed2d8ba306f673a033efdd1c68dc52
jack <janjackson568@gmail.com>
2022-08-04 16:02:47 +0200

    made it work

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#from https://www.youtube.com/watch?v=oDoFvpDftBA
+
 import logging
 import os
 import re
@@ -9,8 +11,8 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 load_dotenv()
 
-SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
-SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+SLACK_APP_TOKEN = "xapp-1-A03S92XELQ5-3895973811299-9c501fd720e54f17a220228c6fce1f39ca766ab54fb1a35f7e2685a75a45fd58"
+SLACK_BOT_TOKEN = "xoxb-3881475683879-3881500756151-awDa886An4t8nY7ZgXPlN4LO"
 
 app = App(token=SLACK_BOT_TOKEN, name="Joke Bot")
 logger = logging.getLogger(__name__)
@@ -31,6 +33,20 @@ def show_random_joke(message, say):
 
     say(text=joke, channel=dm_channel)
 
+@app.message("$")  # type: ignore
+def show_random_joke(message, say):
+    """Send a random pyjoke back"""
+    channel_type = message["channel_type"]
+    if channel_type != "im":
+        return
+
+    dm_channel = message["channel"]
+    user_id = message["user"]
+
+    joke = "Fuck off!"
+    logger.info(f"Sent joke < {joke} > to user {user_id}")
+
+    say(text=joke, channel=dm_channel)
 
 def main():
     handler = SocketModeHandler(app, SLACK_APP_TOKEN)
